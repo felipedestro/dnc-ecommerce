5ae80e3bbe5b9a69e12b16c38bab00ae07a5de70
Felipe Destro <felipe.s.destro@gmail.com>
2024-01-04 17:08:19 -0400

    add component product card

--- src/main.jsx
+++ src/main.jsx
@@ -1,34 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.scss';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.scss";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Login from './view/Login/Login';
-import Home from './view/Home/Home';
-import Products from './view/Products/Products';
-import Pay from './view/Pay/Pay';
+import Login from "./view/Login/Login";
+import Home from "./view/Home/Home";
+import Products from "./view/Products/Products";
+import Pay from "./view/Pay/Pay";
+import { PRODUCTS_MOCK } from "./mock/products.mock";
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element:  <Login />,
-  },
-  {
-    path: "/home",
-    element:  <Home />,
-  },
-  {
-    path: "/products/:productId",
-    element: <Products />,
-  },
-  {
-    path: "/pay/:payId",
-    element:  <Pay />,
-  },
-  
+	{
+		path: "/",
+		element: <Login />,
+	},
+	{
+		path: "/home",
+		element: <Home data={PRODUCTS_MOCK} />,
+	},
+	{
+		path: "/products/:productId",
+		element: <Products data={PRODUCTS_MOCK} />,
+	},
+	{
+		path: "/pay/:payId",
+		element: <Pay data={PRODUCTS_MOCK} />,
+	},
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>
+);
--- src/view/Home/Home.jsx
+++ src/view/Home/Home.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import "./index.scss";
-import { Link } from "react-router-dom";
 import HeaderMenu from "../../components/HeaderMenu/HeaderMenu";
 import Search from "../../components/Search/Search";
-const Home = () => {
+import ProductsCard from "../../components/ProductsCard/ProductsCard";
+
+const Home = ({ data }) => {
 	return (
 		<div className="home">
 			<HeaderMenu />
 			<Search />
+			<div className="home__products">
+				{data.map((product) => (
+					<ProductsCard key={product.id} data={product} />
+				))}
+			</div>
 		</div>
 	);
 };
