c75d8a2301399bb87e3d29533d97fea2ec6adcf9
Felipe Destro <felipe.s.destro@gmail.com>
2024-01-03 20:07:00 -0400

    add components

---
+++ src/components/ProductsCard/ProductsCard.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './index.scss';
+
+function ProductsCard() {
+  return (
+    <div>ProductsCard</div>
+  )
+}
+
+export default ProductsCard;
---
+++ src/components/Search/Serach.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './index.scss';
+
+function Serach() {
+  return (
+    <div>Serach</div>
+  )
+}
+
+export default Serach;
