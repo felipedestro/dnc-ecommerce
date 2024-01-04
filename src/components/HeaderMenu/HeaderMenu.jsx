f14db92d5bca4e25f601b7a265c5fe179be83586
Felipe Destro <felipe.s.destro@gmail.com>
2024-01-04 10:53:57 -0400

    components header-menu

--- src/components/HeaderMenu/HeaderMenu.jsx
+++ src/components/HeaderMenu/HeaderMenu.jsx
@@ -1,10 +1,25 @@
-import React from 'react';
-import './index.scss';
+import React from "react";
+import "./index.scss";
+import logo from "../../assets/logo-dnc.svg";
+import cartLogo from "../../assets/cartshop.svg";
 
 function HeaderMenu() {
-  return (
-    <div>HeaderMenu</div>
-  )
+	return (
+		<header className="header-menu">
+			<img src={logo} alt="Logo DNC" className="header-menu__logo" />
+			<ul>
+				<li>Home</li>
+				<li>Novidade</li>
+				<li>Feminino</li>
+				<li>Masculino</li>
+				<li>Atendimento</li>
+			</ul>
+			<div className="header-menu__cart-shop">
+				<p>Meu Carrinho</p>
+				<img src={cartLogo} alt="Carrinho de compra" />
+			</div>
+		</header>
+	);
 }
 
 export default HeaderMenu;
