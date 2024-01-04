ee7c5a8948f94e3cc689d7f44bcbe7ea32a9a567
Felipe Destro <felipe.s.destro@gmail.com>
2024-01-04 16:16:18 -0400

    components search

--- src/view/Home/Home.jsx
+++ src/view/Home/Home.jsx
@@ -1,12 +1,15 @@
-import React from 'react';
-import './index.scss';
-import { Link } from 'react-router-dom';
-
+import React from "react";
+import "./index.scss";
+import { Link } from "react-router-dom";
+import HeaderMenu from "../../components/HeaderMenu/HeaderMenu";
+import Search from "../../components/Search/Search";
 const Home = () => {
-  return (
-    <div className='home'>
-    </div>
-  )
-}
+	return (
+		<div className="home">
+			<HeaderMenu />
+			<Search />
+		</div>
+	);
+};
 
 export default Home;
