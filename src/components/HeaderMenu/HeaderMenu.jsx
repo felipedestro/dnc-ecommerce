d267a68b2b941c57151acd8d7e917c20eb28f815
Felipe Destro <felipe.s.destro@gmail.com>
2024-01-05 09:42:36 -0400

    view products initial

--- src/components/HeaderMenu/HeaderMenu.jsx
+++ src/components/HeaderMenu/HeaderMenu.jsx
@@ -2,13 +2,16 @@ import React from "react";
 import "./index.scss";
 import logo from "../../assets/logo-dnc.svg";
 import cartLogo from "../../assets/cartshop.svg";
+import { Link } from "react-router-dom";
 
 function HeaderMenu() {
 	return (
 		<header className="header-menu">
 			<img src={logo} alt="Logo DNC" className="header-menu__logo" />
 			<ul>
-				<li>Home</li>
+				<li>
+					<Link to={"/home"}>Home</Link>
+				</li>
 				<li>Novidade</li>
 				<li>Feminino</li>
 				<li>Masculino</li>
--- src/view/Products/Products.jsx
+++ src/view/Products/Products.jsx
@@ -1,10 +1,17 @@
-import React from 'react';
-import './index.scss';
+import React from "react";
+import "./index.scss";
+import HeaderMenu from "../../components/HeaderMenu/HeaderMenu";
+import Search from "../../components/Search/Search";
 
 function Products() {
-  return (
-    <div>Products</div>
-  )
+	return (
+		<div className="products">
+			<HeaderMenu />
+			<div className="products__search">
+				<Search />
+			</div>
+		</div>
+	);
 }
 
 export default Products;
